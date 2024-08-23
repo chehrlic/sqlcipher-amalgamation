b43510f550899778fed091ff416ba5af3c0441f6
Christian Ehrlicher <ch.ehrlicher@gmx.de>
2024-08-23 16:27:24 +0200

    Add sqlcipher-amalgamation v4.6.1

--- create_amalgamation.sh
+++ create_amalgamation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-version=v4.6.0
+version=v4.6.1
 echo Building SQLCipher amalgamation for version $version
 
 rm -rf sqlcipher
@@ -18,6 +18,8 @@ cp LICENSE.md ..
 cd ..
 rm -rf sqlcipher
 
+sqlite_version=`cat sqlite3.c | grep "define SQLITE_VERSION " | cut -d '"' -f 2`
+echo "SQLite version: ${sqlite_version}" > sqlcipher.VERSION
 echo "SQLCiper version: $version" >> sqlcipher.VERSION
 git add sqlite3.c sqlite3.h sqlite3ext.h sqlcipher.VERSION LICENSE.md
 git commit -m "Add sqlcipher-amalgamation $version"
